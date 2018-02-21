08376cc704647a9e1e3d5e00d2159d9e3e7b1e7a
Sakthi <sakthi92@gmail.com>
2018-02-21 15:58:29 +0100

    corrected some mistakes in estimation; didnt resolve the crashing

--- lib/oqpsk_phase_clock_est_cc_impl.cc
+++ lib/oqpsk_phase_clock_est_cc_impl.cc
@@ -22,7 +22,7 @@
 #include "config.h"
 #endif
 #define _USE_MATH_DEFINES
-#define NL0 1000
+#define NL0 100
 
 #include <cstdio>
 #include <cmath>
@@ -69,7 +69,7 @@ namespace gr {
       //q(t) = g(t)exp(1/2*t) (conv) g(t)exp(1/2*t)
       unsigned int d_qtaps_n = 2 * (taps.size() - 1);
       
-      for (int j = 0; j < 2 * (taps.size() - 1); j++) {
+      for (int j = 0; j < d_qtaps_n; j++) {
         d_qtaps[j] = 0;
         for (int k = 0; k < j + 1; k++)
           d_qtaps[j] += taps[j-k] * gr_expj((j-k)/(2*d_sps)) * taps[k] * gr_expj(k/(2*d_sps));
@@ -82,7 +82,7 @@ namespace gr {
       gr_complex *top_aft_filt = (gr_complex *)volk_malloc(sizeof(gr_complex)*NL0, alignment);
       gr_complex *bot_aft_filt = (gr_complex *)volk_malloc(sizeof(gr_complex)*NL0, alignment);  
       
-      set_history(d_qtaps_n);
+      set_history(d_qtaps_n + d_sps + d_sps);
       
       set_output_multiple(1);
     }
@@ -131,13 +131,14 @@ namespace gr {
       float phase_arg;
       int nsamples = (int)std::min(NL0, noi);
       for (int i = 0; i <= nsamples - 1; i++) {
-        phase_arg = M_PI * i / noi;
+        phase_arg = M_PI * i / nsamples;
         sample_phase_top = gr_expj(-phase_arg);
         sample_phase_bot = gr_expj(phase_arg);
         top_b4_filt[i] = in[i] * sample_phase_top;
         bot_b4_filt[i] = in[i] * sample_phase_bot;
       }
       
+      //phase and delay estimation starts here
       d_q_filter->filterN(top_aft_filt, top_b4_filt, nsamples);
       d_q_filter->filterN(bot_aft_filt, bot_b4_filt, nsamples);
       
@@ -147,10 +148,11 @@ namespace gr {
       d_phase = 1 / 4 * (atan2(X.imag(), X.real()) + atan2(Y.imag(), Y.real()));
       d_delay = 1 / (4 * M_PI) * (-atan2(X.imag(), X.real()) + atan2(Y.imag(), Y.real()));
       
+      //correcting the phase and producing outputs
       gr_complex est_phase;
       est_phase = gr_expj(-d_phase);
       std::vector <gr_complex> est_phase_vec (nsamples, est_phase);
-      std::transform(in, in+nsamples, est_phase_vec.begin(), out, std::multiplies<gr_complex>());
+      std::transform(in, in+nsamples, est_phase_vec.begin(), out, std::multiplies<gr_complex>()); //out0
       *out1 = d_phase;
       *out2 = d_delay;
       produce(0, nsamples);
